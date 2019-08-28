c13d84a4bcfdd204aa37237e57ed36db188bb014
Alexander Kuziv <makklays@gmail.com>
2019-08-28 18:23:02 +0300

    Update EmployeesController.php

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -2,21 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
+use App\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class EmployeesController extends Controller
 {
     public function showEmployees()
     {
+        // Only loggined
+        if (!Auth::check()) return redirect('/');
+
         //$employees = DB::select('SELECT * FROM employees ');
-        $employees = DB::table('employees')->paginate(10);
+        //$employees = DB::table('employees')->paginate(10);
+        $employees = Employee::paginate(2);
 
         if (isset($employees) && !empty($employees)) {
             foreach ($employees as $k => &$emp) {
                 // add Name of company
                 if (isset($emp->company_id) && !empty($emp->company_id)) {
-                    $company = DB::selectOne('SELECT * FROM companies WHERE id=? ', [$emp->company_id]);
+                    //$company = DB::selectOne('SELECT * FROM companies WHERE id=? ', [$emp->company_id]);
+                    $company = Company::where('id', $emp->company_id)->first();
+
+                    //dd($company);
+
                     $emp->company = $company->name;
                 } else {
                     $emp->company = '';
@@ -29,20 +40,33 @@ class EmployeesController extends Controller
         ]);
     }
 
-    public function addEmployee(Request $request)
+    public function addEmployee(Request $request) // add customRequest ;)
     {
+        // Only loggined
+        if (!Auth::check()) return redirect('/');
+
         // list of companies
-        $companies = DB::select('SELECT * FROM companies ');
+        //$companies = DB::select('SELECT * FROM companies ');
+        $companies = Company::get();
 
         // add a new employees
         if ($request->isMethod('post')) {
-            $insert = DB::insert('INSERT INTO employees SET `lastname`=?, firstname=?, company_id=?, phone=?, email=?', [
+
+            /*$insert = DB::insert('INSERT INTO employees SET `lastname`=?, firstname=?, company_id=?, phone=?, email=?', [
                 $request->lastname,
                 $request->firstname,
                 $request->company_id,
                 (isset($request->phone) && !empty($request->phone) ? $request->phone : null),
                 (isset($request->email) && !empty($request->email) ? $request->email : null),
-            ]);
+            ]);*/
+
+            $employee = new Employee();
+            $employee->firstname = $request->firstname;
+            $employee->lastname = $request->lastname;
+            $employee->company_id = $request->company_id;
+            $employee->phone = (isset($request->phone) && !empty($request->phone) ? $request->phone : null);
+            $employee->email = (isset($request->email) && !empty($request->email) ? $request->email : null);
+            $employee->save();
 
             return redirect('/employees')->with([
                 'flash_message' => 'Your employee, '.$request->lastname.' '.$request->firstname.' has been add successfully!',
@@ -55,20 +79,32 @@ class EmployeesController extends Controller
         ]);
     }
 
-    public function edit(Request $request, $id)
+    public function edit(Request $request, $id) // add CustomRequest :)
     {
+        // Only loggined
+        if (!Auth::check()) return redirect('/');
+
         if (!isset($id) || !empty($id)) redirect('/employees');
 
         // update data of company
         if ($request->isMethod('post') && isset($request->lastname) && !empty($request->lastname)) {
-            DB::update('UPDATE `employees` SET `lastname`=?, `firstname`=?, `company_id`=?, `phone`=?, `email`=? WHERE id=?', [
+
+            /*DB::update('UPDATE `employees` SET `lastname`=?, `firstname`=?, `company_id`=?, `phone`=?, `email`=? WHERE id=?', [
                 $request->lastname,
                 $request->firstname,
                 $request->company_id,
                 (isset($request->phone) && !empty($request->phone) ? $request->phone : null),
                 (isset($request->email) && !empty($request->email) ? $request->email : null),
                 $id
-            ]);
+            ]);*/
+
+            $employee = Employee::where('id', $id);
+            $employee->firstname = $request->firstname;
+            $employee->lastname = $request->lastname;
+            $employee->company_id = $request->company_id;
+            $employee->phone = (isset($request->phone) && !empty($request->phone) ? $request->phone : null);
+            $employee->email = (isset($request->email) && !empty($request->email) ? $request->email : null);
+            $employee->save();
 
             return redirect('employees')->with([
                 'flash_message' => 'Your employee, '.$request->lastname.' '.$request->firstname.' has been update successfully!',
@@ -77,9 +113,12 @@ class EmployeesController extends Controller
         }
 
         // get companies
-        $companies = DB::select('SELECT * FROM companies ');
+        //$companies = DB::select('SELECT * FROM companies ');
+        $companies = Company::get();
+
         // get employee
-        $employee = DB::selectOne('SELECT * FROM employees WHERE id=?', [$id]);
+        //$employee = DB::selectOne('SELECT * FROM employees WHERE id=?', [$id]);
+        $employee = Employee::where('id', $id)->first();
 
         return view('employees.edit', [
             'employee' => $employee,
@@ -89,6 +128,9 @@ class EmployeesController extends Controller
 
     public function delete($id)
     {
+        // Only loggined
+        if (!Auth::check()) return redirect('/');
+
         if (!isset($id) || empty($id)) {
             return redirect('employees')->with([
                 'flash_message' => 'Error! Don\'t have ID in url',
@@ -97,7 +139,8 @@ class EmployeesController extends Controller
         }
 
         // get employee
-        $employee = DB::selectOne('SELECT * FROM employees WHERE id = ?', [$id]);
+        //$employee = DB::selectOne('SELECT * FROM employees WHERE id = ?', [$id]);
+        $employee = Employee::where('id', $id)->first();
 
         // delete employee
         if (isset($employee) && !empty($employee)) {
@@ -105,7 +148,8 @@ class EmployeesController extends Controller
             $fullname = $employee->lastname.' '.$employee->firstname;
 
             // delete empoyee
-            DB::delete('DELETE FROM employees WHERE id = ?', [$id]);
+            //DB::delete('DELETE FROM employees WHERE id = ?', [$id]);
+            Employee::where('id', $id)->delete();
 
             return redirect('employees')->with([
                 'flash_message' => 'Your employee, '.$fullname.' has been delete successfully!',
