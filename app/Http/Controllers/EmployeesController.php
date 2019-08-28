acace5d9ab252ee4c89142f858c4694a073cc584
Alexander Kuziv <makklays@gmail.com>
2019-08-28 18:25:12 +0300

    Update EmployeesController.php

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 /**
- * На примере Компаний - простой пример перализации CRUD (Create Read Update Delete)
+ * На примере Сотрудников - простой пример перализации CRUD (Create Read Update Delete)
  **/
 class EmployeesController extends Controller
 {
